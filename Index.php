9da96b3169d6c8128980945d79449ef36ad5db74
Erikadlv <56602045+Erikadlv@users.noreply.github.com>
2019-11-17 18:31:23 -0600

    Se agregaron las imagenes, el archivo css y el html Termine el login y estoy por enlazarlo a un html, a traves de php No he creado el archivo html donde se enlazara, hasta aqui mi reporte Joaquin Se agrego un carrousel 3D

--- Index.php
+++ Index.php
@@ -1,4 +1,5 @@
 <?php
+header("Location: http://localhost:63342/proyecto-final-Erikadlv/PaginaPrincipal.html?_ijt=gmg0m71668a13glbpqatldo90p");
 
     $dbhost="localhost";
     $dbuser="root";
@@ -16,10 +17,11 @@
     $nr= mysqli_num_rows($query);
     
     if($nr == 1){
-    /*echo "Bienenido: ".$nombre;*/
+    echo "Bienenido: ".$nombre;
         header('location: ');
+        /*echo "<head><meta http-equiv='refresh' content='2; url='PaginaPrincipal.html'></head>";*/
     }
-    else if($nr==o) {
+    else if($nr==0) {
         echo "El usuario/contraseña es invalido";
     }
 ?>
